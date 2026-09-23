891ba4108307e955879eaa3b422c26fb8b7f7f6d
Shanmugapriyan126 <35727800+Shanmugapriyan126@users.noreply.github.com>
2026-09-23 11:45:39 +0530

    Enhance Vite config with Tailwind CSS and server options
    Refactor Vite configuration to include Tailwind CSS and add alias and server settings.

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,23 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
 
-export default defineConfig({
-  plugins: [react()],
-  base: '/RKCMS/',
-})
+export default defineConfig(() => {
+  return {
+    plugins: [react(), tailwindcss()],
+
+    base: '/RKCMS/',
+
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, '.'),
+      },
+    },
+
+    server: {
+      hmr: process.env.DISABLE_HMR !== 'true',
+      watch: process.env.DISABLE_HMR === 'true' ? null : {},
+    },
+  };
+});
